575e3d999d85165972fd1bae142060b0d74ea0b8
Alexander Manbeck <ajm9330@psu.edu>
2025-05-03 05:59:16 -0400

    changes

--- card-list.js
+++ card-list.js
@@ -33,51 +33,14 @@ export class CardList extends DDDSuper(LitElement) {
       super.styles,
       css`
         :host {
-          display: flex;
-          flex-direction: column;
-          color: var(--ddd-theme-primary);
-          background-color: var(--ddd-theme-accent);
-          font-family: var(--ddd-font-navigation);
-          max-width: 300px;
-          min-height: 450px;
-          background-color: var(--ddd-theme-default-coalyGray);
-          color: white;
-          border-radius: var(--ddd-radius-sm);
-          overflow: hidden;
-        }
-        .card {
-          display: flex;
-          flex-direction: column;
-          height: 100%;
-        }
-        .content {
-          padding: 16px;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-        }
-        h3 {
-          margin: 0 0 0.5rem;
-          font-size: 1.5rem;
-        }
-        p {
-          font-size: 1rem;
-          margin: 0 0 1rem;
-        }
-        a.button {
           display: block;
-          text-align: center;
-          text-decoration: none;
-          color: var(--ddd-theme-default-white);
-          background-color: var(--ddd-theme-default-nittanyNavy);
-          padding: 0.5rem 1rem;
-          margin: 16px;
-          border-radius: var(--ddd-radius-xs);
+          padding: 2rem 1.5rem 1.5rem;
         }
-        img {
-          width: 100%;
-          height: 160px;
-          object-fit: cover;
+        .card-list {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: space-between;
+          gap: 1rem;
         }
       `,
     ];
@@ -85,7 +48,11 @@ export class CardList extends DDDSuper(LitElement) {
 
   // Lit render the HTML
   render() {
-    return html``;
+    return html`
+      <div class="card-list">
+        <slot></slot>
+      </div>
+    `;
   }
 }
 
